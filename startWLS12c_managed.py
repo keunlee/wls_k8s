8667a084c17032d29a8ce7496376e1f18e61d549
Radu Dobrinescu <radu.dobrinescu@gmail.com>
2018-02-02 13:43:06 +0000

    fixed start

--- startWLS12c_managed.py
+++ startWLS12c_managed.py
@@ -8,49 +8,79 @@ hostname = socket.gethostname()
 admin_url = os.environ["ADMIN_HOSTNAME"]+':'+AdminServerPort
 ip_addr = os.popen("/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'").read()
 
-def deleteMachine(hostname):
+def addMachine():
  edit()
  startEdit(10000,20000,'true')
  cd('/')
- cd('Machines')
- delete(hostname)
+ create(hostname,'UnixMachine')
+ cd('Machines/'+hostname)
+ cd('NodeManager/'+hostname)
+ set('ListenAddress',ip_addr)
+ set('NMType','Plain')
  save()
  activate()
 
-def stopManagedServer(server):
-  try:
-   shutdown(server,'Server',force='true')
-  except:
-   print "Server has been already shutdown"
+def changeMachineIP():
+ edit()
+ startEdit(10000,20000,'true')
+ cd('/')
+ cd('Machines/'+hostname+'/NodeManager/'+hostname)
+ set('ListenAddress',ip_addr)
+ save()
+ activate()
 
-def scaleInCluster():
+def createMS():
  edit()
  startEdit(10000,20000,'true')
  cd('/')
  cd('Clusters/'+DynamicClusterName+'/DynamicServers/'+DynamicClusterName)
- serverCount = cmo.getMaximumDynamicServerCount()-1
+ serverCount = cmo.getMaximumDynamicServerCount()+1
  set('MaximumDynamicServerCount',serverCount)
- print "The number of Dynamic Servers in the cluster has been decreased to: "+str(serverCount)
+ print "The number of Dynamic Servers in the cluster has been increased to: "+str(serverCount)
  save()
  activate()
 
-print "Cleaning up the domain configuration after scaling down the cluster..."
-connect(AdminUser,AdminServerPassword,'http://'+admin_url);
+def startNM():
+ #try:
+ #  nmConnect(AdminUser,AdminServerPassword,str(ip_addr),5556,DomainName,DomainPath+DomainName,'plain')
+ # except:
+ #  print "It seems Node Manager is not running. Starting Node Manager..."
+   print "Starting the Node Manager on this machine..."
+   startNodeManager(NodeManagerHome=DomainPath+DomainName+'/nodemanager',PropertiesFile=DomainPath+DomainName+'/nodemanager/nodemanager.properties',verbose=false)
+   print "Waiting 60 seconds for Node Manager to start..."
+   Thread.sleep(60000)
+   try:
+    nmConnect(AdminUser,AdminServerPassword,str(ip_addr),5556,DomainName,DomainPath+DomainName,'plain')
+   except:
+    print "Unable to connect to Node Manager. Resuming configuration and trying again later..."
 
-svrs = cmo.getServers()
-domainRuntime()
-for server in svrs:
-  if server.getName() != 'AdminServer':
-    serverConfig()
-    machine = server.getMachine();
-    if machine.getName() == hostname:
-     print "Name of the server to remove is: "+server.getName()
-     state(server.getName(), 'Server')
-     stopManagedServer(server.getName())
-     state(server.getName(), 'Server')
-     print "Removing the server instance from the Dynamic Cluster"
-     scaleInCluster()
-     print "Deleting the Unix Machine from the Domain configuration"
-     deleteMachine(machine.getName())
-exit()
+print "IP address of this Docker container is "+ip_addr
+startNM()
+connect(AdminUser,AdminServerPassword,'http://'+admin_url);
+nmEnroll(DomainPath+DomainName,DomainPath+DomainName+'/nodemanager');
+
+cd('/')
+cd('Machines')
+machines = ls()
+if (machines.find(hostname) != -1):
+    print "Machine with name "+hostname+" already exists in this domain"
+    changeMachineIP()
+    cd('/')
+    cd('Clusters/'+DynamicClusterName+'/DynamicServers/'+DynamicClusterName)
+    serverCount = cmo.getMaximumDynamicServerCount()
+    print serverCount
+    #nmConnect(AdminUser,AdminServerPassword,str(ip_addr),5556,DomainName,DomainPath+DomainName,'plain')
+    start(ManagedServerPrefix+str(serverCount),'Server')
+else:
+    print "Adding a new machine named "+hostname+" to the domain..."
+    addMachine()
+    print "Adding a new Dynamic Server to the <"+DynamicClusterName+"> cluster"
+    createMS()
+    cd('/')
+    cd('Clusters/'+DynamicClusterName+'/DynamicServers/'+DynamicClusterName)
+    serverCount = cmo.getMaximumDynamicServerCount()
+    print serverCount
+    #nmConnect(AdminUser,AdminServerPassword,str(ip_addr),5556,DomainName,DomainPath+DomainName,'plain')
+    start(ManagedServerPrefix+str(serverCount),'Server')
+exit();
 
