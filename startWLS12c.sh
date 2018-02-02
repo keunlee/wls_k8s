b7620a3e9a6b192826fecadeee7ff2fde1c7d578
Radu Dobrinescu <radu.dobrinescu@gmail.com>
2018-02-02 11:44:25 +0000

    fix script file name

--- startWLS12c.sh
+++ startWLS12c.sh
@@ -4,10 +4,10 @@ echo "Starting " ${SERVER_TYPE}
 
 if [[ "${SERVER_TYPE}" == "AdminServer" ]]; then
    echo "Calling script to start the AdminServer."
-   nohup java -Djava.security.egd=file:/dev/./urandom weblogic.WLST $SCRIPT_PATH/startWLS12c_admin.jy &
+   nohup java -Djava.security.egd=file:/dev/./urandom weblogic.WLST $SCRIPT_PATH/startWLS12c_admin.py &
    else if [[ "${SERVER_TYPE}" == "ManagedServer" ]]; then
    echo "Calling script to start ManagedServer"
-   nohup java -Djava.security.egd=file:/dev/./urandom weblogic.WLST $SCRIPT_PATH/startWLS12c_managed.jy &
+   nohup java -Djava.security.egd=file:/dev/./urandom weblogic.WLST $SCRIPT_PATH/startWLS12c_managed.py &
         else
                 echo "SERVER_TYPE needs to be defined to either <AdminServer> or <ManagedServer>"
                 exit
