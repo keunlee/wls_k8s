61385da069999cb538f6235475db9bcff0a04c01
Radu Dobrinescu <radu.dobrinescu@gmail.com>
2018-02-02 11:44:19 +0000

    fix script file name

--- scaleInWLS12.sh
+++ scaleInWLS12.sh
@@ -2,7 +2,7 @@
 
 echo "Scaling In the WebLogic cluster "
    echo "Calling script to stop and remove Managed Server from cluster."
-   java -Djava.security.egd=file:/dev/./urandom weblogic.WLST $SCRIPT_PATH/stopWLS12c_managed.jy
+   java -Djava.security.egd=file:/dev/./urandom weblogic.WLST $SCRIPT_PATH/stopWLS12c_managed.py
    echo "Deleting NodeManager leftover lock files..."
    rm -rf DomainPath+DomainName+'/nodemanager/*.lck'
 
